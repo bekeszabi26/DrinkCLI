311bfb29a9ad7e4a3e037625d486412bd09e6ca7
bekeszabi26 <145983566+bekeszabi26@users.noreply.github.com>
2024-05-21 21:06:43 +0200

    Add files via upload

---
+++ drink/drink/drink/Citizen.cs
@@ -0,0 +1,70 @@
+﻿namespace drink
+{
+    internal class Citizen
+    {
+        
+        public int Age { get; set; }
+        public int CitizenId { get; set; }
+        public bool Drinking { get; set; }
+        public int DrinkingWeekdays { get; set; }
+        public int DrinkingWeekends { get; set; }
+        public bool Gender { get; set; }
+        public int NationalityId { get; set; }
+        public string NationalityName { get; set; }
+        public string NetIncome { get; set; }
+        public int PopulationGroupID { get; set; }
+        public string PopulationGroupName { get; set; }
+        public string SchoolDropout { get; set; }
+        public string State { get; set; }
+        public bool Treatment { get; set; }
+
+        public Citizen(string row, int id)
+        {
+            string[] t = row.Split(';');
+            Age = int.Parse(t[0]);
+            CitizenId = id;
+            Drinking = t[1] == "Yes" ? true : false;
+            DrinkingWeekdays = t[2] == "NO" ? -1 : int.Parse(t[2]);
+            DrinkingWeekends = t[3] == "NO" ? -1 : int.Parse(t[3]);
+            Gender = t[4] == "Male" ? true : false;
+            NationalityId = int.Parse(t[5]);
+            NationalityName = t[6];
+            NetIncome = t[7];
+            PopulationGroupID = int.Parse(t[8]);
+            PopulationGroupName = t[9];
+            SchoolDropout = t[10];
+            State = t[11];
+            Treatment = t[12] == "1" ? true : false;
+        }
+
+        public override string ToString()
+        {
+            return $"\tCitizen Id: {CitizenId}\n" +
+                $"\tAge: {Age}\n" +
+                $"\tDrinking: {(Drinking? "Yes" : "No")}\n" +
+                $"\tDrinking on weekdays: {(DrinkingWeekdays ==  -1 ? "NO" : DrinkingWeekdays)}\n" +
+                $"\tDrinking on weekends: {(DrinkingWeekends == -1 ? "NO" : DrinkingWeekends)}\n" +
+                $"\tGender: {(Gender? "Male" : "Female")}\n" +
+                $"\tNationality: {NationalityName}\n" +
+                $"\tNetIncome: {NetIncome}\n" +
+                $"\tPopulation Group: {PopulationGroupName}\n" +
+                $"\tDrop out from school: {SchoolDropout}\n" +
+                $"\tState: {State}\n" +
+                $"\tTreatment: {Treatment}\n" +
+                $"\tAverage Drinking: {AverageDrinking()}\n";
+        }
+
+        private string AverageDrinking()
+        {
+            if (DrinkingWeekdays == -1 || DrinkingWeekends == -1)
+            {
+                return "NaN";
+            }
+            else
+            {
+                return $"{((double)DrinkingWeekdays + DrinkingWeekends) / 7:0.00}";
+            }
+        }
+
+    }
+}
---
+++ drink/drink/drink/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+
+namespace drink
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Citizen> citizens = new List<Citizen>();
+
+            using StreamReader sr = new StreamReader(@"..\..\..\src\drink.txt");
+            _ = sr.ReadLine();
+
+            int citizenId = 1;
+            while (!sr.EndOfStream)
+            {
+                citizens.Add(new Citizen(sr.ReadLine(), citizenId));
+                citizenId++;
+            }
+
+            //5. feladat
+            Console.WriteLine("5. feladat");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(citizens[i]);
+            }
+            //5. feladat vége
+
+            //6. feladat
+            Console.WriteLine("6. feladat");
+            var f6 = citizens.Where(c => c.Drinking == true && c.NationalityName == "Birthright citizen" && c.State == "Florida" && c.Gender == true && c.NetIncome != "Refused" && c.NetIncome != "Unknown" && c.NetIncome != "Under 1800" && c.NetIncome != "Above 32400").ToList();
+
+            if (f6.Count > 0)
+            {
+                foreach (var item in f6)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var min = f6.Min(f => int.Parse(f.NetIncome.Split("to")[0].Trim()));
+
+                Console.WriteLine($"A kiválogatott állampolgárok lehető legkisebb jövedelme USA dollárban: {min}");
+            }
+            //6. feladat vége
+
+            //7. feladat
+            var f7 = citizens.Where(c => (c.Age >= 18 && c.Age <= 65) && c.PopulationGroupName == "Asian" && c.Drinking == true).OrderBy(c => c.State).ToList();
+            using StreamWriter sw = new StreamWriter(@"..\..\..\src\asian.txt");
+            Console.WriteLine("7. feladat: fájlba írás");
+            foreach (var item in f7)
+            {
+                sw.WriteLine($"{item.Age},{item.State},{item.SchoolDropout}");
+            }
+            //7. feladat vége
+
+            Console.ReadLine();
+        }
+    }
+}
